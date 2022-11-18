11d16fa6308d44ec5a1cc694c3c3989d8c403da4
Anas7823 <anaselkhiat78@gmail.com>
2022-11-18 14:59:01 +0100

    architecture

--- REACT/m2l/src/index.js
+++ REACT/m2l/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../src/style/index.css';
-import App from '../src/componoent/App';
+import './style/index.css';
+import App from './componoent/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
